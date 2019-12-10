6c25a8a885b9d860a1e588f4344818820450f5dc
haomiaocqut <yhaozhang@163.com>
2019-12-10 14:58:54 +0800

    Delete [2]pre_trainRun.py

--- [2]pre_trainRun.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-dataName = "Yelp"
-
-data_set = '../Data/' + dataName + '/' + dataName +'_rating.npz'
-pre_output = '../Data/' + dataName + '/' + dataName +'_rating_e10.npz'
-
-subprocess.call(['python', 'pre_train.py', '--gpu', '0', '--dataset', data_set, '--output', pre_output])
